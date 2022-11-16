92d5db2f3e7ee15b5f3516c461876077c7cc39bd
AkiiLucky <2581026417@qq.com>
2022-11-16 05:40:03 -0800

    add fetchPage

--- project/project1/scudb_initial/src/buffer/buffer_pool_manager.cpp
+++ project/project1/scudb_initial/src/buffer/buffer_pool_manager.cpp
@@ -46,7 +46,40 @@ BufferPoolManager::~BufferPoolManager() {
  * 4. Update page metadata, read page content from disk file and return page
  * pointer
  */
-Page *BufferPoolManager::FetchPage(page_id_t page_id) { return nullptr; }
+Page *BufferPoolManager::FetchPage(page_id_t page_id) {
+    lock_guard<mutex> lck(latch_);
+    //1 search the page from buffer(page table).
+    //1.1 if the page exist in buffer(page table), pin the page and return immediately
+    Page *target = nullptr;
+    if (page_table_->Find(page_id, target))
+    {
+        target->pin_count_++;
+        //erase the page from lru_replacer in order to avoid to the page replaced
+        replacer_->Erase(target);
+        return target;
+    }
+    //1.2 if the page not exist in buffer(page table), find a free page or replace an old page
+    target = GetVictimPage();
+    if (target == nullptr) //not find available page (all page pinned), then return nullptr
+    {
+        return target;
+    }
+    //2 if the page for replacement is dirty, write it back to disk.
+    if (target->is_dirty_)
+    {
+        disk_manager_->WritePage(target->GetPageId(), target->data_);
+    }
+    //3 Delete the old page for replacement from the buffer(page table)
+    page_table_->Remove(target->GetPageId());
+    //4 Update page metadata, read page content from disk file and insert the new page into buffer(page table)
+    disk_manager_->ReadPage(page_id, target->data_);
+    target->pin_count_ = 1;
+    target->is_dirty_ = false;
+    target->page_id_= page_id;
+    page_table_->Insert(page_id, target);
+
+    return target; //return page pointer
+}
 
 /*
  * Implementation of unpin page
@@ -55,7 +88,21 @@ Page *BufferPoolManager::FetchPage(page_id_t page_id) { return nullptr; }
  * dirty flag of this page
  */
 bool BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty) {
-  return false;
+    lock_guard<mutex> lck(latch_);
+    Page *target = nullptr;
+    page_table_->Find(page_id, target);
+    if (target == nullptr) {
+        return false;
+    }
+    target->is_dirty_ = is_dirty;
+    if (target->GetPinCount() <= 0) {
+        return false;
+    }
+    target->pin_count_ -= 1;
+    if (target->pin_count_ == 0) {
+        replacer_->Insert(target);
+    }
+    return true;
 }
 
 /*
@@ -85,4 +132,28 @@ bool BufferPoolManager::DeletePage(page_id_t page_id) { return false; }
  * into page table. return nullptr if all the pages in pool are pinned
  */
 Page *BufferPoolManager::NewPage(page_id_t &page_id) { return nullptr; }
+
+
+
+//get a free page from free list or lru_replacer
+Page *BufferPoolManager::GetVictimPage() {
+    Page *page = nullptr;
+    if (!free_list_->empty()) //free_list have free page
+    {
+        page = free_list_->front();
+        free_list_->pop_front();
+        assert(page->GetPageId() == INVALID_PAGE_ID);
+    }
+    else  //no free page in free_list
+    {
+        if (replacer_->Size() == 0) //can not replace any page, because all page pinned by process
+        {
+            return nullptr;
+        }
+        replacer_->Victim(page); //replace a page from replacer
+    }
+    assert(page->GetPinCount() == 0);
+    return page;
+}
+
 } // namespace scudb
