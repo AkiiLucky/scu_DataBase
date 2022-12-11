4c597829aa542d0f734d4cf8622507921a79e248
AkiiLucky <2581026417@qq.com>
2022-12-11 01:24:32 -0800

    update index iterator

--- project/project2/scudb_initial/src/include/index/index_iterator.h
+++ project/project2/scudb_initial/src/include/index/index_iterator.h
@@ -1,37 +1,3 @@
-//**
-// * index_iterator.h
-// * For range scan of b+ tree
-// */
-//#pragma once
-//#include "page/b_plus_tree_leaf_page.h"
-//
-//namespace scudb {
-//
-//#define INDEXITERATOR_TYPE
-//  IndexIterator<KeyType, ValueType, KeyComparator>
-//
-//
-//
-//INDEX_TEMPLATE_ARGUMENTS
-//class IndexIterator {
-//public:
-//  // you may define your own constructor based on your member variables
-//  IndexIterator();
-//  ~IndexIterator();
-//
-//  bool isEnd();
-//
-//  const MappingType &operator*();
-//
-//  IndexIterator &operator++();
-//
-//private:
-//  // add your own private member variables here
-//};
-//
-//} // namespace scudb
-
-
 /**
  * index_iterator.h
  * For range scan of b+ tree
@@ -41,51 +7,30 @@
 
 namespace scudb {
 
-#define INDEXITERATOR_TYPE                                                     \
-  IndexIterator<KeyType, ValueType, KeyComparator>
+#define INDEXITERATOR_TYPE IndexIterator<KeyType, ValueType, KeyComparator>
 
-    INDEX_TEMPLATE_ARGUMENTS
-    class IndexIterator {
-    public:
-        // you may define your own constructor based on your member variables
-        IndexIterator(B_PLUS_TREE_LEAF_PAGE_TYPE *leaf, int index, BufferPoolManager *bufferPoolManager);
-        ~IndexIterator();
+INDEX_TEMPLATE_ARGUMENTS
+class IndexIterator {
+public:
+  // you may define your own constructor based on your member variables
+  IndexIterator(B_PLUS_TREE_LEAF_PAGE_TYPE *leafPage, int indexInPage, BufferPoolManager *bufferPoolManager);
 
-        bool isEnd(){
-            return (leaf_ == nullptr);
-        }
+  ~IndexIterator();
 
-        const MappingType &operator*() {
-            return leaf_->GetItem(index_);
-        }
+  bool isEnd();
 
-        IndexIterator &operator++() {
-            index_++;
-            if (index_ >= leaf_->GetSize()) {
-                page_id_t next = leaf_->GetNextPageId();
-                UnlockAndUnPin();
-                if (next == INVALID_PAGE_ID) {
-                    leaf_ = nullptr;
-                } else {
-                    Page *page = bufferPoolManager_->FetchPage(next);
-                    page->RLatch();
-                    leaf_ = reinterpret_cast<B_PLUS_TREE_LEAF_PAGE_TYPE *>(page->GetData());
-                    index_ = 0;
-                }
-            }
-            return *this;
-        }
+  const MappingType &operator*();
 
-    private:
-        // add your own private member variables here
-        void UnlockAndUnPin() {
-            bufferPoolManager_->FetchPage(leaf_->GetPageId())->RUnlatch();
-            bufferPoolManager_->UnpinPage(leaf_->GetPageId(), false);
-            bufferPoolManager_->UnpinPage(leaf_->GetPageId(), false);
-        }
-        int index_;
-        B_PLUS_TREE_LEAF_PAGE_TYPE *leaf_;
-        BufferPoolManager *bufferPoolManager_;
-    };
+  INDEXITERATOR_TYPE &operator++();
+
+private:
+    // add your own private member variables here
+    void UnlockAndUnPin() ;
+
+    int indexInPage_;
+    B_PLUS_TREE_LEAF_PAGE_TYPE *leafPage_;
+    BufferPoolManager *bufferPoolManager_;
+};
 
 } // namespace scudb
+
--- project/project2/scudb_initial/src/index/index_iterator.cpp
+++ project/project2/scudb_initial/src/index/index_iterator.cpp
@@ -1,30 +1,3 @@
-//**
-// * index_iterator.cpp
-// */
-//#include <cassert>
-//
-//#include "index/index_iterator.h"
-//
-//namespace scudb {
-//
-//*
-// * NOTE: you can change the destructor/constructor method here
-// * set your own input parameters
-// */
-//INDEX_TEMPLATE_ARGUMENTS
-//INDEXITERATOR_TYPE::IndexIterator() {}
-//
-//INDEX_TEMPLATE_ARGUMENTS
-//INDEXITERATOR_TYPE::~IndexIterator() {}
-//
-//template class IndexIterator<GenericKey<4>, RID, GenericComparator<4>>;
-//template class IndexIterator<GenericKey<8>, RID, GenericComparator<8>>;
-//template class IndexIterator<GenericKey<16>, RID, GenericComparator<16>>;
-//template class IndexIterator<GenericKey<32>, RID, GenericComparator<32>>;
-//template class IndexIterator<GenericKey<64>, RID, GenericComparator<64>>;
-//
-//} // namespace scudb
-
 /**
  * index_iterator.cpp
  */
@@ -39,15 +12,62 @@ namespace scudb {
  * set your own input parameters
  */
     INDEX_TEMPLATE_ARGUMENTS
-    INDEXITERATOR_TYPE::IndexIterator(B_PLUS_TREE_LEAF_PAGE_TYPE *leaf, int index, BufferPoolManager *bufferPoolManager)
-            : index_(index),leaf_(leaf), bufferPoolManager_(bufferPoolManager){}
+    INDEXITERATOR_TYPE::IndexIterator(B_PLUS_TREE_LEAF_PAGE_TYPE *leafPage, int indexInPage, BufferPoolManager *bufferPoolManager)
+            : indexInPage_(indexInPage),leafPage_(leafPage), bufferPoolManager_(bufferPoolManager){}
+
 
     INDEX_TEMPLATE_ARGUMENTS
     INDEXITERATOR_TYPE::~IndexIterator() {
-        if (leaf_ != nullptr) {
-            UnlockAndUnPin();
-        }
+        if (leafPage_ != nullptr) UnlockAndUnPin(); //unpin this leaf page
     }
+
+
+    INDEX_TEMPLATE_ARGUMENTS
+    bool INDEXITERATOR_TYPE::isEnd() {
+        return (leafPage_ == nullptr);
+    }
+
+
+    INDEX_TEMPLATE_ARGUMENTS
+    const MappingType& INDEXITERATOR_TYPE::operator*() {
+        return leafPage_->GetItem(indexInPage_);
+    }
+
+
+    INDEX_TEMPLATE_ARGUMENTS
+    INDEXITERATOR_TYPE &INDEXITERATOR_TYPE::operator++() {
+        indexInPage_++;
+        int pageSize = leafPage_->GetSize();
+        if (indexInPage_ >= pageSize) //if next key & value pair is in next page
+        {
+            page_id_t nextPageId = leafPage_->GetNextPageId();
+            //unlock and unpin this page
+            UnlockAndUnPin();
+            if (nextPageId == INVALID_PAGE_ID) leafPage_ = nullptr; //this page is last page
+            else
+            {
+                //fetch next page from buffer pool
+                Page *nextPage = bufferPoolManager_->FetchPage(nextPageId);
+                //get reading lock from next page
+                nextPage->RLatch();
+                //convert page type to B_PLUS_TREE_LEAF_PAGE_TYPE
+                leafPage_ = reinterpret_cast<B_PLUS_TREE_LEAF_PAGE_TYPE *>(nextPage->GetData());
+                //reset index in page is 0
+                indexInPage_ = 0;
+            }
+        }
+        return *this;
+    }
+
+
+    INDEX_TEMPLATE_ARGUMENTS
+    void INDEXITERATOR_TYPE::UnlockAndUnPin() {
+        bufferPoolManager_->FetchPage(leafPage_->GetPageId())->RUnlatch();
+        bufferPoolManager_->UnpinPage(leafPage_->GetPageId(), false);
+        bufferPoolManager_->UnpinPage(leafPage_->GetPageId(), false);
+    }
+
+
 
 
     template class IndexIterator<GenericKey<4>, RID, GenericComparator<4>>;
